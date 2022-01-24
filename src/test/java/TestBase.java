36cd308260c18d38c80d1246c1847b2aad56d38d
Evgeniia Miroshina <evgeniia.miroshina@t-systems.com>
2022-01-24 20:30:00 +0300

    close WebDriver

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -28,7 +28,7 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        //Selenide.closeWebDriver();
+        Selenide.closeWebDriver();
         Attach.addVideo();
 
     }
